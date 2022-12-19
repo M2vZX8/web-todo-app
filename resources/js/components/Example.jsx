4b0da183974e547e9289a3b3503f40c7b6f1d2fa
M2vZX8 <agsw-pc@yahoo.co.jp>
2022-12-19 15:05:29 +0900

    Update Example.jsx

--- resources/js/components/Example.jsx
+++ resources/js/components/Example.jsx
@@ -7,7 +7,7 @@ function Example() {
             <div className="row justify-content-center">
                 <div className="col-md-8">
                     <div className="card">
-                        <div className="card-header">Example Component</div>
+                        <div className="card-header">Test</div>
 
                         <div className="card-body">I'm an example component!</div>
                     </div>
@@ -20,11 +20,5 @@ function Example() {
 export default Example;
 
 if (document.getElementById('example')) {
-    const Index = ReactDOM.createRoot(document.getElementById("example"));
-
-    Index.render(
-        <React.StrictMode>
-            <Example/>
-        </React.StrictMode>
-    )
+    ReactDOM.render(<Example />, document.getElementById('example'));
 }
